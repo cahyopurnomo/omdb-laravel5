b52ed8d2f352fa341ad27745158b0d2781515c81
Cahyo <cahyo@lyrid.co.id>
2025-08-15 05:30:38 +0700

    First commit

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect()->route('movies.index');
+});
 
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
